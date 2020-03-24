e2796f9c15f9ce9d77422688ba6b370caec99f23
jpd21122012 <slipknot_jpd@hotmail.com>
2020-03-23 20:30:21 -0600

    Program.cs Ready to Predict Sample Image

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using IntroductionToMLML.Model;
 
 namespace IntroductionToML
 {
@@ -6,7 +8,14 @@ namespace IntroductionToML
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Predicting Image...");
+            ModelInput input = new ModelInput();
+            input.ImageSource = "https://ep01.epimg.net/verne/imagenes/2019/10/21/articulo/1571661515_258318_1571663010_noticia_normal.jpg";
+
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"Result:\n Category: {result.Prediction} | Score: {result.Score.Max()*100}%");
+
         }
     }
 }
